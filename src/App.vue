056391cebc25d79b5ba9081fe31d4d3f51dfba2a
Benjaminj Jones <benjaminj@dev-it.info>
2023-12-30 13:53:42 +0200

    getting there

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,90 @@
 <template>
-  <nav class="flex items-center justify-between px-4">
+  <nav class="flex items-center justify-between p-4 bg-gray-800 text-white">
     <!-- Logo -->
-    <div class="flex flex-col text-center">
-      <img src="https://i.ibb.co/M1knWxw/ESSI.webp" alt="" class="h-20" />
-      <h1 class="essi-logo -mt-4 text-2xl font-semibold text-white">ESSI</h1>
-    </div>
+    <img
+      src="https://i.ibb.co/M1knWxw/ESSI.webp"
+      alt="ESSI Logo"
+      class="h-12"
+    />
 
-    <!-- Navigation Links (centered) -->
+    <!-- Navigation Links -->
     <div class="hidden md:flex space-x-4">
-      <router-link to="/" class="hover:text-blue-500">Home</router-link>
-      <router-link to="/apod" class="hover:text-blue-500">APOD</router-link>
-      <router-link to="/earth-image" class="hover:text-blue-500"
+      <router-link to="/" class="hover:text-gray-300">Home</router-link>
+      <router-link to="/apod" class="hover:text-gray-300">APOD</router-link>
+      <router-link to="/earth-image" class="hover:text-gray-300"
         >Earth Image</router-link
       >
-      <router-link to="/epic-image" class="hover:text-blue-500"
+      <router-link to="/epic-image" class="hover:text-gray-300"
         >EPIC Image</router-link
       >
-      <!-- Add more links as needed -->
     </div>
 
-    <!-- Dark Theme Toggle (right) -->
-    <button @click="toggleDarkTheme" class="p-2">Toggle Dark Theme</button>
+    <!-- Theme Switcher -->
+    <div class="relative">
+      <input
+        type="checkbox"
+        id="theme-switcher"
+        class="sr-only"
+        @change="toggleDarkTheme"
+        :checked="isDarkTheme"
+      />
+      <label
+        for="theme-switcher"
+        class="bg-gray-600 rounded-full p-1 w-20 h-7 flex items-center justify-between cursor-pointer"
+      >
+        <i class="fas fa-sun text-yellow-500"></i>
+        <span
+          class="bg-white rounded-full block w-5 h-5 absolute transform transition-transform"
+          :class="{ 'translate-x-12': isDarkTheme }"
+          :style="{ left: isDarkTheme ? '0.6rem' : '0.25rem' }"
+        ></span>
+        <i class="fas fa-moon text-indigo-300"></i>
+      </label>
+    </div>
   </nav>
 
   <router-view />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "App",
-  methods: {
-    toggleDarkTheme() {
-      const html = document.documentElement;
-      const currentTheme = html.getAttribute("data-theme");
-      const newTheme = currentTheme === "dark" ? "light" : "dark";
-      html.setAttribute("data-theme", newTheme);
-    },
+  setup() {
+    const isDarkTheme = ref(
+      localStorage.getItem("theme") === "dark" ||
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    );
+
+    // Watch for changes on the system preferences
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        isDarkTheme.value = event.matches;
+        applyTheme();
+      });
+
+    // Apply the initial theme
+    applyTheme();
+
+    // Method to toggle the theme
+    function toggleDarkTheme() {
+      isDarkTheme.value = !isDarkTheme.value;
+      applyTheme();
+    }
+
+    // Apply theme changes
+    function applyTheme() {
+      document.documentElement.setAttribute(
+        "data-theme",
+        isDarkTheme.value ? "dark" : "light"
+      );
+      // Update the local storage
+      localStorage.setItem("theme", isDarkTheme.value ? "dark" : "light");
+    }
+
+    return { isDarkTheme, toggleDarkTheme };
   },
 });
 </script>
