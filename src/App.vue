c3a678291e02496c9cd14984c136f84e1c148363
92Infinitus92 <benjaminj@dev-it.info>
2023-12-30 18:12:04 +0200

    fixed the display of the content

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,84 @@
 <template>
-  <nav class="bg-gray-800 text-white p-4 flex justify-between items-center">
-    <!-- Logo -->
-    <div class="flex flex-col">
-      <img
-        src="https://i.ibb.co/M1knWxw/ESSI.webp"
-        alt="ESSI Logo"
-        class="h-24"
-      />
-      <h1 class="essi-logo text-xl font-semibold text-center -mt-5">ESSI</h1>
-    </div>
-
-    <!-- Navigation Links -->
-    <div
-      class="md:flex md:items-center md:justify-center w-fit md:w-auto bg-gray-800 md:bg-transparent"
-      :class="{ block: isMobileMenuOpen, hidden: !isMobileMenuOpen }"
-    >
-      <router-link
-        to="/"
-        class="block px-2 py-2 hover:text-gray-300 text-center"
-        >Home</router-link
-      >
-      <router-link
-        to="/apod"
-        class="block px-2 py-2 hover:text-gray-300 text-center"
-        >APOD</router-link
-      >
-      <router-link
-        to="/earth-image"
-        class="block px-2 py-2 hover:text-gray-300 text-center"
-        >Earth Image</router-link
-      >
-      <router-link
-        to="/epic-image"
-        class="block px-2 py-2 hover:text-gray-300 text-center"
-        >EPIC Image</router-link
-      >
-    </div>
-
-    <!-- Theme Switcher and Sign In Button -->
-    <div class="flex items-center justify-between md:w-1/5 w-60">
-      <!-- Theme Switcher -->
-      <div class="order-2 md:order-1 relative">
-        <input
-          type="checkbox"
-          id="theme-switcher"
-          class="sr-only"
-          @change="toggleDarkTheme"
-          :checked="isDarkTheme"
+  <div>
+    <nav class="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <!-- Logo -->
+      <div class="flex flex-col">
+        <img
+          src="https://i.ibb.co/M1knWxw/ESSI.webp"
+          alt="ESSI Logo"
+          class="h-24"
         />
-        <label
-          for="theme-switcher"
-          class="bg-gray-600 rounded-full p-1 w-20 h-7 flex items-center justify-between cursor-pointer px-3"
-        >
-          <i class="fas fa-sun text-yellow-500"></i>
-          <span
-            class="bg-white rounded-full block w-5 h-5 absolute transform transition-transform"
-            :class="{ 'translate-x-12': isDarkTheme }"
-            :style="{ left: isDarkTheme ? '0.50rem' : '0.60rem' }"
-          ></span>
-          <i class="fas fa-moon text-indigo-300"></i>
-        </label>
+        <h1 class="essi-logo text-xl font-semibold text-center -mt-5">ESSI</h1>
       </div>
 
-      <!-- Sign In Button -->
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded order-2"
+      <!-- Navigation Links -->
+      <div
+        class="md:flex md:items-center md:justify-center w-fit md:w-auto bg-gray-800 md:bg-transparent"
+        :class="{ block: isMobileMenuOpen, hidden: !isMobileMenuOpen }"
       >
-        Sign In
-      </button>
+        <router-link
+          to="/"
+          class="block px-2 py-2 hover:text-gray-300 text-center"
+          >Home</router-link
+        >
+        <router-link
+          to="/apod"
+          class="block px-2 py-2 hover:text-gray-300 text-center"
+          >APOD</router-link
+        >
+        <router-link
+          to="/earth-image"
+          class="block px-2 py-2 hover:text-gray-300 text-center"
+          >Earth Image</router-link
+        >
+        <router-link
+          to="/epic-image"
+          class="block px-2 py-2 hover:text-gray-300 text-center"
+          >EPIC Image</router-link
+        >
+      </div>
 
-      <!-- Mobile Menu Button -->
-      <button @click="toggleMobileMenu" class="ml-4 md:hidden p-2 rounded">
-        <i class="fas fa-bars" v-if="!isMobileMenuOpen"></i>
-        <i class="fas fa-times" v-else></i>
-      </button>
-    </div>
+      <!-- Theme Switcher and Sign In Button -->
+      <div class="flex items-center justify-between md:w-1/5 w-60">
+        <!-- Theme Switcher -->
+        <div class="order-2 md:order-1 relative">
+          <input
+            type="checkbox"
+            id="theme-switcher"
+            class="sr-only"
+            @change="toggleDarkTheme"
+            :checked="isDarkTheme"
+          />
+          <label
+            for="theme-switcher"
+            class="bg-gray-600 rounded-full p-1 w-20 h-7 flex items-center justify-between cursor-pointer px-3"
+          >
+            <i class="fas fa-sun text-yellow-500"></i>
+            <span
+              class="bg-white rounded-full block w-5 h-5 absolute transform transition-transform"
+              :class="{ 'translate-x-12': isDarkTheme }"
+              :style="{ left: isDarkTheme ? '0.50rem' : '0.60rem' }"
+            ></span>
+            <i class="fas fa-moon text-indigo-300"></i>
+          </label>
+        </div>
 
-    <!-- Mobile Menu -->
-    <!-- <div
+        <!-- Sign In Button -->
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded order-2"
+        >
+          Sign In
+        </button>
+
+        <!-- Mobile Menu Button -->
+        <button @click="toggleMobileMenu" class="ml-4 md:hidden p-2 rounded">
+          <i class="fas fa-bars" v-if="!isMobileMenuOpen"></i>
+          <i class="fas fa-times" v-else></i>
+        </button>
+      </div>
+
+      <!-- Mobile Menu -->
+      <!-- <div
       v-if="isMobileMenuOpen"
       class="absolute top-32 left-0 w-full bg-gray-800 md:hidden"
     >
@@ -98,7 +99,10 @@
         >EPIC Image</router-link
       >
     </div> -->
-  </nav>
+    </nav>
+    <!-- Router View: Components for the current route will be rendered here -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
--- src/views/EarthImage.vue
+++ src/views/EarthImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mt-52">
     <h1>Earth Image</h1>
     <!-- The Earth Image content will be rendered here -->
   </div>
