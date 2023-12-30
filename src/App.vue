6d5b8a9f6763ff13b99bb4dff1d7679a4f72a27d
Benjaminj Jones <benjaminj@dev-it.info>
2023-12-30 15:09:48 +0200

    looking better on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,57 @@
 <template>
   <nav
-    class="flex items-center justify-between px-12 bg-gray-800 text-white pb-2"
+    class="flex flex-wrap items-center justify-between bg-gray-800 text-white p-4"
   >
-    <!-- Logo -->
-    <div class="flex flex-col">
+    <!-- Logo on the left -->
+    <div class="flex-shrink-0">
       <img
         src="https://i.ibb.co/M1knWxw/ESSI.webp"
         alt="ESSI Logo"
-        class="h-24"
+        class="h-12 md:h-24"
       />
-      <h1 class="essi-logo text-xl font-semibold text-center -mt-5">ESSI</h1>
+      <h1 class="text-lg md:text-xl font-semibold text-center -mt-2 md:-mt-5">
+        ESSI
+      </h1>
     </div>
 
+    <!-- Mobile Menu Button -->
+    <button
+      class="md:hidden p-2 focus:outline-none focus:bg-gray-700 rounded"
+      @click="toggleMobileMenu"
+    >
+      <i class="fas fa-bars" v-if="!isMobileMenuOpen"></i>
+      <i class="fas fa-times" v-else></i>
+    </button>
+
     <!-- Navigation Links -->
-    <div class="hidden md:flex space-x-4">
-      <router-link to="/" class="hover:text-gray-300">Home</router-link>
-      <router-link to="/apod" class="hover:text-gray-300">APOD</router-link>
-      <router-link to="/earth-image" class="hover:text-gray-300"
+    <div
+      class="md:flex md:items-center md:justify-center w-full md:w-auto order-3 md:order-2 bg-gray-800 md:bg-transparent"
+      :class="{ block: isMobileMenuOpen, hidden: !isMobileMenuOpen }"
+    >
+      <router-link
+        to="/"
+        class="block px-4 py-2 hover:text-gray-300 text-center"
+        >Home</router-link
+      >
+      <router-link
+        to="/apod"
+        class="block px-4 py-2 hover:text-gray-300 text-center"
+        >APOD</router-link
+      >
+      <router-link
+        to="/earth-image"
+        class="block px-4 py-2 hover:text-gray-300 text-center"
         >Earth Image</router-link
       >
-      <router-link to="/epic-image" class="hover:text-gray-300"
+      <router-link
+        to="/epic-image"
+        class="block px-4 py-2 hover:text-gray-300 text-center"
         >EPIC Image</router-link
       >
     </div>
 
     <!-- Theme Switcher -->
-    <div class="relative">
+    <div class="relative order-2 md:order-3">
       <input
         type="checkbox"
         id="theme-switcher"
@@ -35,13 +61,12 @@
       />
       <label
         for="theme-switcher"
-        class="bg-gray-600 rounded-full p-1 w-20 h-7 flex items-center justify-between cursor-pointer px-3"
+        class="bg-gray-600 rounded-full p-1 w-14 h-7 flex items-center justify-between cursor-pointer"
       >
         <i class="fas fa-sun text-yellow-500"></i>
         <span
-          class="bg-white rounded-full block w-5 h-5 absolute transform transition-transform"
-          :class="{ 'translate-x-12': isDarkTheme }"
-          :style="{ left: isDarkTheme ? '0.5rem' : '0.55rem' }"
+          class="bg-white rounded-full block w-5 h-5 transform transition-transform"
+          :class="{ 'translate-x-7': isDarkTheme }"
         ></span>
         <i class="fas fa-moon text-indigo-300"></i>
       </label>
@@ -61,35 +86,25 @@ export default defineComponent({
       localStorage.getItem("theme") === "dark" ||
         window.matchMedia("(prefers-color-scheme: dark)").matches
     );
+    const isMobileMenuOpen = ref(false);
 
-    // Watch for changes on the system preferences
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", (event) => {
-        isDarkTheme.value = event.matches;
-        applyTheme();
-      });
+    function toggleMobileMenu() {
+      isMobileMenuOpen.value = !isMobileMenuOpen.value;
+    }
 
-    // Apply the initial theme
-    applyTheme();
-
-    // Method to toggle the theme
     function toggleDarkTheme() {
       isDarkTheme.value = !isDarkTheme.value;
-      applyTheme();
+      const theme = isDarkTheme.value ? "dark" : "light";
+      document.documentElement.setAttribute("data-theme", theme);
+      localStorage.setItem("theme", theme);
     }
 
-    // Apply theme changes
-    function applyTheme() {
-      document.documentElement.setAttribute(
-        "data-theme",
-        isDarkTheme.value ? "dark" : "light"
-      );
-      // Update the local storage
-      localStorage.setItem("theme", isDarkTheme.value ? "dark" : "light");
-    }
-
-    return { isDarkTheme, toggleDarkTheme };
+    return {
+      isDarkTheme,
+      toggleDarkTheme,
+      isMobileMenuOpen,
+      toggleMobileMenu,
+    };
   },
 });
 </script>
